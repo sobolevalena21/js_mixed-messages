048e097ee38762d08cabce8a2736dbf2ef8409ca
sandrasoboleva <sobolevalena21@gmail.com>
2022-09-23 12:24:15 -0400

    Add 'Yey-Ney'

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const parts = ['wheels', 'wipers', 'horn', 'driver']
 const sounds = ['round and round', 'swish-swish-swish', 'beep-beep-beep', 'Move on back']
 
@@ -7,18 +6,12 @@ function songCompile() {
     let randomNum = Math.floor(Math.random()*4);
     let part=parts[randomNum];
     let sound = sounds[randomNum];
-    console.log(`The ${part} on the bus go ${sound}, ${sound}, ${sound}, the ${part} on the bus go ${sound} all through the town`)
+   console.log(`The ${part} on the bus go ${sound}, ${sound}, ${sound}, the ${part} on the bus go ${sound} all through the town`);
+    let randomNum1 = Math.floor(Math.random()*4);
+    let sound1 = sounds[randomNum1];
+    console.log(`The ${part} on the bus go ${sound1}, ${sound1}, ${sound1}, the ${part} on the bus go ${sound1} all through the town.`);
+    sound1===sound ? console.log('Yey!!!') : console.log('Neeey :/');
 }
 
 songCompile()
-
-function songCompileMix() {
-    let randomNum = Math.floor(Math.random()*4);
-    let randomNum1 = Math.floor(Math.random()*4);
-    let part=parts[randomNum];
-    let sound = sounds[randomNum1];
-    console.log(`The ${part} on the bus go ${sound}, ${sound}, ${sound}, the ${part} on the bus go ${sound} all through the town.`)
-}
-
-songCompileMix()
 
